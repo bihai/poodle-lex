5bd3df5b49b20813495bfea1b09113a79fa68237
Parker Michaels <parkertomatoes@gmail.com>
2014-05-26 12:18:56 -0500

    Adding missing source files

---
+++ Generator/CommandArguments.py
@@ -0,0 +1,52 @@
+# Copyright (C) 2014 Parker Michaels
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a 
+# copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the 
+# Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# DEALINGS IN THE SOFTWARE.
+
+import sys
+import argparse
+
+class Command(object):
+    def __init__(self, type, arguments):
+        self.type = type
+        self.arguments = arguments
+
+def handle():
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'list-minimizers':
+            return 'list-minimizers', None
+        if sys.argv[1] == 'list-languages':
+            return 'list-languages', None
+    
+    # Parse complex options
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("RULES_FILE", help="Rules file")
+    arg_parser.add_argument("OUTPUT_DIR", help="Containing directory for output files")
+    arg_parser.add_argument("-e", "--encoding", help="Rules file encoding", default="utf-8")
+    arg_parser.add_argument("-n", "--print-nfa", help="Print a graph of the NFA of the ruleset to a .dot file", metavar="DOT_FILE")
+    arg_parser.add_argument("-d", "--print-dfa", help="Print a graph of the non-minimized DFA of the ruleset to a .dot file", metavar="DOT_FILE")
+    arg_parser.add_argument("-m", "--print-min-dfa", help="Print a graph of the minimized DFA of the ruleset to a .dot file", metavar="DOT_FILE")
+    arg_parser.add_argument("-i", "--minimizer", help="Minimizer algorithm to use. Use '--action=list-minimizers' for list of options", default="hopcroft", metavar="ALGORITHM")
+    arg_parser.add_argument("-l", "--language", help="Output programming language. Use '--action=list-languages' for list of options", default=None)
+    arg_parser.add_argument("-c", "--class-name", help="The name of the lexical analyzer class to generate", default=None)
+    arg_parser.add_argument("-f", "--file-name", help="The base name of the generated source files", default=None)
+    arg_parser.add_argument("-s", "--namespace", help="The namespace name of the lexical analyzer class to generate", default=None)
+    arguments = arg_parser.parse_args()
+
+    return 'build', arguments
+    
---
+++ Generator/LanguagePlugins/Executor.py
@@ -0,0 +1,95 @@
+# Copyright (C) 2014 Parker Michaels
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a 
+# copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the 
+# Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# DEALINGS IN THE SOFTWARE.
+
+from ..Emitter.FileTemplate import FileTemplate
+from ..Emitter.PluginTemplate import TemplateToken
+import shutil
+import os.path
+import os
+
+class Executor(object):
+    """
+    Class which contains main code to generate a lexical analyzer using a 
+    language plugin.
+    """
+    def __init__(self, language_plugin, plugin_files_directory, output_directory):
+        """
+        @param language_plugin: The emitter object created by the 
+            representing the emitter plugin
+        @param plugin_files_directory: String containing path to the files used by
+            the emitter plugin
+        @param output_directory: The output folder into which the lexical 
+            analyzer should be generated
+        """
+        self.language_plugin = language_plugin
+        self.plugin_files_directory = plugin_files_directory
+        self.output_directory = output_directory
+        
+    def execute(self):
+        """
+        Helper function to generate the entire lexical analyzer
+        """
+        self.create_directories()
+        self.copy_files()
+        self.generate_files()
+        
+    def create_directories(self):
+        """
+        Create sub-directories in the output directory
+        """
+        try:
+            if not os.path.exists(self.output_directory):
+                raise Exception("Output directory not found")
+
+            for directory_name in self.language_plugin.get_output_directories():
+                real_directory_name = os.path.join(self.output_directory, directory_name)
+                if not os.path.exists(real_directory_name):
+                    os.mkdir(real_directory_name)
+        except Exception as e:
+            raise Exception("While creating directories, " + str(e))
+            
+    def copy_files(self):
+        """
+        Copy non-generated files into the output directory
+        """
+        try:
+            for file in self.language_plugin.get_files_to_copy():
+                shutil.copy(os.path.join(self.plugin_files_directory, file), os.path.join(self.output_directory, file))
+        except Exception as e:
+            raise Exception("While coping files, " + str(e))
+            
+    def generate_files(self):
+        """
+        Create generated files in the output directory and call back into the 
+        plugin to process tokens
+        """
+        try:
+            for template_file, output_file in self.language_plugin.get_files_to_generate():
+                real_template_file = os.path.join(self.plugin_files_directory, template_file)
+                real_output_file = os.path.join(self.output_directory, output_file)
+                template_token = TemplateToken(template_file)
+                for stream, token, indent in FileTemplate(real_template_file, real_output_file):
+                    template_token.token = token
+                    template_token.stream = stream
+                    template_token.indent = indent
+                    self.language_plugin.process(template_token)
+        except Exception as e:
+            raise Exception("While generating files, " + str(e))
+            

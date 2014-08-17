366860103ec14eb331c828ae18397367b868bec7
Parker Michaels <parkertomatoes@gmail.com>
2014-08-17 09:02:01 -0500

    Updating lexical analyzer test to support change to DFA IDs

--- Test/TestLexicalAnalyzer.py
+++ Test/TestLexicalAnalyzer.py
@@ -6,6 +6,12 @@ import unittest
 from Generator import Automata
 from Generator.CoverageSet import CoverageSet
 from Generator.RulesFile import AST, NonDeterministicIR, DeterministicIR
+
+def hash_ids(*ids):
+    rules = set()
+    for id in ids:
+        rules.add(hash((id.lower(), frozenset(), (None, None))))
+    return frozenset(rules)
    
 class TestDFAEquivalency(unittest.TestCase):
     def test_lexical_analyzer(self):
@@ -26,100 +32,100 @@ class TestDFAEquivalency(unittest.TestCase):
         dfa_s = [Automata.DeterministicState() for i in xrange(18)]
         
         #Start state
-        dfa_s[0].ids = frozenset(("float", "whitespace", "keyword", "integer", "quote", "identifier", "newline"))
+        dfa_s[0].ids = hash_ids("float", "whitespace", "keyword", "integer", "quote", "identifier", "newline")
         
         #quote
         dfa_s[0].edges[dfa_s[2]] = CoverageSet([(34, 34)])
         dfa_s[2].is_final = False
-        dfa_s[2].ids = frozenset(("quote",))
+        dfa_s[2].ids = hash_ids("quote")
         dfa_s[2].edges[dfa_s[10]] = CoverageSet([(92, 92)])
         dfa_s[2].edges[dfa_s[2]] = CoverageSet([(1, 33), (35, 91), (93, 0x10FFFF)])
         dfa_s[10].is_final = False
-        dfa_s[10].ids = frozenset(("quote",))
+        dfa_s[10].ids = hash_ids("quote")
         dfa_s[10].edges[dfa_s[2]] = CoverageSet([(1, 0x10FFFF)])
         dfa_s[2].edges[dfa_s[9]] = CoverageSet([(34, 34)])
         dfa_s[9].is_final = True
-        dfa_s[9].final_ids = frozenset(("quote",))
-        dfa_s[9].ids = frozenset(("quote",))
+        dfa_s[9].final_ids = hash_ids("quote")
+        dfa_s[9].ids = hash_ids("quote")
         
         #newline
         dfa_s[0].edges[dfa_s[3]] = CoverageSet([(13, 13)])
         dfa_s[0].edges[dfa_s[1]] = CoverageSet([(10, 10)])
         dfa_s[3].is_final = True
-        dfa_s[3].ids = frozenset(("newline",))
-        dfa_s[3].final_ids = frozenset(("newline",))
+        dfa_s[3].ids = hash_ids("newline")
+        dfa_s[3].final_ids = hash_ids("newline")
         dfa_s[3].edges[dfa_s[1]] = CoverageSet([(10, 10)])
         dfa_s[1].is_final = True
-        dfa_s[1].ids = frozenset(("newline",))
-        dfa_s[1].final_ids = frozenset(("newline",))
+        dfa_s[1].ids = hash_ids("newline")
+        dfa_s[1].final_ids = hash_ids("newline")
         
         #identifier
         dfa_s[0].edges[dfa_s[5]] = CoverageSet([(65, 74), (76, 90), (97, 106), (108, 122)])
         dfa_s[5].is_final = True
-        dfa_s[5].ids = frozenset(("identifier",))
-        dfa_s[5].final_ids = frozenset(("identifier",))
+        dfa_s[5].ids = hash_ids("identifier")
+        dfa_s[5].final_ids = hash_ids("identifier")
         dfa_s[5].edges[dfa_s[5]] = CoverageSet([(48, 57), (65, 90), (95, 95), (97, 122), (0x20ac, 0x20ac)])
         
         #integer
         dfa_s[0].edges[dfa_s[4]] = CoverageSet([(48, 57)])
         dfa_s[4].is_final = True
-        dfa_s[4].ids = frozenset(("integer","float"))
-        dfa_s[4].final_ids = frozenset(("integer",))
+        dfa_s[4].ids = hash_ids("integer","float")
+        dfa_s[4].final_ids = hash_ids("integer")
         dfa_s[4].edges[dfa_s[4]] = CoverageSet([(48, 57)])
         dfa_s[4].edges[dfa_s[11]] = CoverageSet([(46, 46)])
         
         #float
         dfa_s[0].edges[dfa_s[6]] = CoverageSet([(46, 46)])
         dfa_s[6].is_final = False
-        dfa_s[6].ids = frozenset(("float",))
+        dfa_s[6].ids = hash_ids("float")
         dfa_s[6].edges[dfa_s[11]] = CoverageSet([(48, 57)])
         dfa_s[11].is_final = True
-        dfa_s[11].ids = frozenset(("float",))
-        dfa_s[11].final_ids = frozenset(("float",))
+        dfa_s[11].ids = hash_ids("float")
+        dfa_s[11].final_ids = hash_ids("float")
         dfa_s[11].edges[dfa_s[11]] = CoverageSet([(48, 57)])
         
         #whitespace
         dfa_s[0].edges[dfa_s[7]] = CoverageSet([(9, 9), (32, 32)])
         dfa_s[7].is_final = True
-        dfa_s[7].ids = frozenset(("whitespace",))
-        dfa_s[7].final_ids = frozenset(("whitespace",))
+        dfa_s[7].ids = hash_ids("whitespace")
+        dfa_s[7].final_ids = hash_ids("whitespace")
         dfa_s[7].edges[dfa_s[7]] = CoverageSet([(9, 9), (32, 32)])
         
         #keyword
         dfa_s[0].edges[dfa_s[8]] = CoverageSet([(75, 75), (107, 107)])
         dfa_s[8].is_final = True
-        dfa_s[8].ids = frozenset(("identifier", "keyword"))
-        dfa_s[8].final_ids = frozenset(("identifier",))
+        dfa_s[8].ids = hash_ids("identifier", "keyword")
+        dfa_s[8].final_ids = hash_ids("identifier")
         dfa_s[8].edges[dfa_s[12]] = CoverageSet([(0x20ac, 0x20ac)])
         dfa_s[8].edges[dfa_s[5]] = CoverageSet([(48, 57), (65, 90), (95, 95), (97, 122)])
         dfa_s[12].is_final = True
-        dfa_s[12].ids = frozenset(("identifier", "keyword"))
-        dfa_s[12].final_ids = frozenset(("identifier",))
+        dfa_s[12].ids = hash_ids("identifier", "keyword")
+        dfa_s[12].final_ids = hash_ids("identifier")
         dfa_s[12].edges[dfa_s[13]] = CoverageSet([(121, 121), (89, 89)])
         dfa_s[12].edges[dfa_s[5]] = CoverageSet([(48, 57), (65, 88), (90, 90), (95, 95), (97, 120), (122, 122), (0x20ac, 0x20ac)])
         dfa_s[13].is_final = True
-        dfa_s[13].ids = frozenset(("identifier", "keyword"))
-        dfa_s[13].final_ids = frozenset(("identifier",))
+        dfa_s[13].ids = hash_ids("identifier", "keyword")
+        dfa_s[13].final_ids = hash_ids("identifier")
         dfa_s[13].edges[dfa_s[14]] = CoverageSet([(119, 119), (87, 87)])
         dfa_s[13].edges[dfa_s[5]] = CoverageSet([(48, 57), (65, 86), (88, 90), (95, 95), (97, 118), (120, 122), (0x20ac, 0x20ac)])
         dfa_s[14].is_final = True
-        dfa_s[14].ids = frozenset(("identifier", "keyword"))
-        dfa_s[14].final_ids = frozenset(("identifier",))
+        dfa_s[14].ids = hash_ids("identifier", "keyword")
+        dfa_s[14].final_ids = hash_ids("identifier")
         dfa_s[14].edges[dfa_s[15]] = CoverageSet([(111, 111), (79, 79)])
         dfa_s[14].edges[dfa_s[5]] = CoverageSet([(48, 57), (65, 78), (80, 90), (95, 95), (97, 110), (112, 122), (0x20ac, 0x20ac)])
         dfa_s[15].is_final = True
-        dfa_s[15].ids = frozenset(("identifier", "keyword"))
-        dfa_s[15].final_ids = frozenset(("identifier",))
+        dfa_s[15].ids = hash_ids("identifier", "keyword")
+        dfa_s[15].final_ids = hash_ids("identifier")
         dfa_s[15].edges[dfa_s[16]] = CoverageSet([(114, 114), (82, 82)])
         dfa_s[15].edges[dfa_s[5]] = CoverageSet([(48, 57), (65, 81), (83, 90), (95, 95), (97, 113), (115, 122), (0x20ac, 0x20ac)])
         dfa_s[16].is_final = True
-        dfa_s[16].ids = frozenset(("identifier", "keyword"))
-        dfa_s[16].final_ids = frozenset(("identifier",))
+        dfa_s[16].ids = hash_ids("identifier", "keyword")
+        dfa_s[16].final_ids = hash_ids("identifier")
         dfa_s[16].edges[dfa_s[17]] = CoverageSet([(100, 100), (68, 68)])
         dfa_s[16].edges[dfa_s[5]] = CoverageSet([(48, 57), (65, 67), (69, 90), (95, 95), (97, 99), (101, 122), (0x20ac, 0x20ac)])
         dfa_s[17].is_final = True
-        dfa_s[17].ids = frozenset(("identifier", "keyword"))
-        dfa_s[17].final_ids = frozenset(("identifier","keyword"))
+        dfa_s[17].ids = hash_ids("identifier", "keyword")
+        dfa_s[17].final_ids = hash_ids("identifier","keyword")
         dfa_s[17].edges[dfa_s[5]] = CoverageSet([(48, 57), (65, 90), (95, 95), (97, 122), (0x20ac, 0x20ac)])
         
         expected_dfa.start_state = dfa_s[0]
